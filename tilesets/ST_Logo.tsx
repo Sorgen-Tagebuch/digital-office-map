9ec9cadb41899b80ecfc371cb6c7d6f0272080d7
Daniel Kemen <dk@software-design.de>
2023-02-25 13:26:00 +0100

    .

--- tilesets/ST_Logo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="ST_Logo" tilewidth="32" tileheight="32" tilecount="4" columns="4">
- <image source="../../../../Downloads/tagebuch_logo_black 1.png" width="128" height="33"/>
-</tileset>
